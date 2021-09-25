b0ccf7851b724d8c64cb93de525d9a52126d2181
silvio-coelho <infinitygroupparaguay@gmail.com>
2021-09-25 13:02:48 -0300

    controller caixa

--- app/Http/Livewire/CaixaController.php
+++ app/Http/Livewire/CaixaController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Artisan;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Caixa;
-
+use Illuminate\Support\Facades\Auth;
 
 class CaixaController extends Component
 {
@@ -42,4 +42,139 @@ class CaixaController extends Component
         }
         
     }
+
+
+
+    public function updateSearch()
+    {
+        $this->gotoPage(1);
+    }
+
+
+
+    public function doAction($action)
+    {
+        $this->resetInput();
+        $this->action = $action;
+    }
+
+
+
+    public function resetInput()
+    {
+        $this->descricao = '';
+        $this->tipo = 'Escolher';
+        $this->valor = '';
+        $this->comprovante= '';
+        $this->selected_id = null;
+        $this->action = -1;
+        $this->search = '';
+    }
+
+
+    public function edit($id)
+    {
+        $record = Caixa::find($id);
+        $this->descricao = $record->descricao;
+        $this->tipo =  $record->tipo;
+        $this->valor =  $record->valor;
+        $this->comprovante=  $record->comprovante;
+        $this->selected_id = $id;
+        $this->action =  $record->action;
+    }
+
+
+    public function storeOrUpdate()
+    {
+        $this->validate([
+            'tipo' = > 'not_in:Escolher'
+        ]);
+
+        $this->validate([
+            'tipo' => 'required',
+            'valor' => 'required',
+            'descricao' => 'required'
+        ]);
+
+        /* novo registro */
+        if($this->selected_id <=0 )
+        {
+            $caixa = Caixa::create([
+                'valor' => $this->valor,
+                'tipo' => $this->tipo,
+                'descricao' => $this->descricao,
+                'user_id' => Auth::user()->id // auth()->user()->id
+            ]);
+
+            if($this->comprovante)
+            {
+                $image = $this->comprovante;
+                $fileName = time().'.' . explode('/', explode(':', substr($image, 0, strpos($image, ';'))))[1][1];
+                $moved = \Image::make($image)->save('images/'.$fileName);
+
+                if($moved)
+                {
+                    $caixa->comprovante = $fileName;
+                    $caixa->save();
+                }
+            }
+        } 
+        else 
+        {
+            $record = Caixa::find($this->selected_id);
+            
+            $record = Caixa::update([
+                'valor' => $this->valor,
+                'tipo' => $this->tipo,
+                'descricao' => $this->descricao,
+                'user_id' => Auth::user()->id 
+            ]);
+
+            if($this->comprovante)
+            {
+                $image = $this->comprovante;
+                $fileName = time().'.' . explode('/', explode(':', substr($image, 0, strpos($image, ';'))))[1][1];
+                $moved = \Image::make($image)->save('images/'.$fileName);
+
+                if($moved)
+                {
+                    $record->comprovante = $fileName;
+                    $record->save();
+                }
+            }
+        }
+
+
+        if($this->selected_id)
+            $this->emit('msgok', 'Movimento de caixa atualizado com sucesso!');
+        else
+            $this->emit('msgok', 'Movimento de caixa criado com sucesso!');
+
+        $this->resetInput();
+        
+    }
+
+
+    protected $listeners = [
+        'deliteRow' => 'destroy',
+        'fileUpload' => 'handleFileUpload'
+    ];
+
+
+    protected function handleFileUpload()
+    {
+        $this->comprovante = $imageData;
+    }
+
+
+    public function destroy($id)
+    {
+        if($id)
+        {
+            $record = Caixa::where('id', $id);
+            $record->delete();
+            $this->resetInput();
+        }
+    }
+
 }
